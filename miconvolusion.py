1afb3598f072605c0daabd561cc2043a7bc93935
a01745865 <69483330+a01745865@users.noreply.github.com>
2020-10-28 18:02:02 -0600

    Convolusion Hecha

--- miconvolusion.py
+++ miconvolusion.py
@@ -2,29 +2,44 @@
 import numpy as np
 
 def convolusion(A,B):
-    C =[] 
-    contador = 0
-    for c in range(len(A)):
-        suma = 0
-        for i in range(len(B)):
-            for a in range(len(B[i])):
-                if contador == 0:
-                    suma += A[i][a]*B[i][a]
-                if contador == 1:
-                    suma += A[i][a+1]*B[i][a]
-                if contador == 2:
-                    suma += A[i+1][a]*B[i][a]
-                if contador == 3:
-                    suma += A[i+1][a+1]*B[i][a]
-        contador += 1
-        C.append(suma)
-    print(C)
-Matriz1 = [[6,9,0,3],[8,4,9,1],[4,1,3,12],[3,2,1,100]]
-Filtro = [[1,0,2],[5,0,9],[6,2,1]]
+    
+    C2 = []
+    columnasM = len(A[0])
+    renglonesM = len(A)
+    columnasFiltro = len(B[0])
+    renglonesFiltro = len(B)
+        
+    contadorRen = 0
+    while renglonesFiltro + contadorRen <= renglonesM:
+        C =[]
+        contadorCol = 0
+        while columnasFiltro + contadorCol <=  columnasM:
+            for c in range(len(A)):        
+                suma = 0
+                for i in range(len(B)):
+                    for a in range(len(B[i])):
+                        suma += A[i+contadorRen][a+contadorCol]*B[i][a]
+            contadorCol += 1
+            C.append(suma)
+        C2.append(C)
+        contadorRen += 1
+    return C2
+    
+Matriz1 = [[1,2,3,4,5,6],[7,8,9,10,11,12],[0,0,1,16,17,18],[0,1,0,7,23,24],[1,7,6,5,4,3]]
+Filtro = [[1,1,1],[0,0,0],[2,10,3]]
+
+Matriz2 = [[6,9,0,3],[8,4,9,1],[4,1,3,12],[3,2,1,100]]
+Filtro2 = [[1,0,2],[5,0,9],[6,2,1]]
+
 
 A = np.array(Matriz1)
 B = np.array(Filtro)
 
-convolusion(A,B)
+C = np.array(Matriz2)
+D = np.array(Filtro2)
+
+print(convolusion(A, B))
+
+print(convolusion(C, D))
 
 
